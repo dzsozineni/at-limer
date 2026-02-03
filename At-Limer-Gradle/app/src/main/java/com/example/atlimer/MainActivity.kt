8d9e4a16220ecd75840fca5bc4e6449ac6af9525
dzsozineni <nagymeno20@icloud.com>
2026-02-03 13:56:38 +0100

    Add files via upload

---
+++ At-Limer-Gradle/app/src/main/java/com/example/atlimer/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.example.atlimer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(b: Bundle?) {
+        super.onCreate(b)
+        setContentView(R.layout.activity_main)
+    }
+}
