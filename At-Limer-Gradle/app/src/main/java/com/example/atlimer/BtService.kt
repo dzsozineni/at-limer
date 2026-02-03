98925423e7cbabb6259fee2cc30c87f582b65a0f
dzsozineni <nagymeno20@icloud.com>
2026-02-03 13:57:47 +0100

    Delete At-Limer-Gradle directory

--- At-Limer-Gradle/app/src/main/java/com/example/atlimer/BtService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.atlimer
-import android.app.Service
-import android.content.Intent
-import android.os.IBinder
-class BtService : Service() {
-    override fun onBind(intent: Intent?): IBinder? = null
-}
