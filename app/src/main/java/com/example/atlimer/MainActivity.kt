01804c841f09ebac78a18dbf50a87f2e63c7842b
dzsozineni <nagymeno20@icloud.com>
2026-02-03 18:40:16 +0100

    Delete app directory

--- app/src/main/java/com/example/atlimer/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.atlimer
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(b: Bundle?) {
-        super.onCreate(b)
-        setContentView(R.layout.activity_main)
-    }
-}
